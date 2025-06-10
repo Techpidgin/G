66d8dacfa97fd815aaeacb7c9b5dc12586adc8f8
dhanzy <holuwadanzy@gmail.com>
2025-06-10 17:22:02 +0100

    Updated Dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -66,10 +66,10 @@ function DialogContent({
         <DialogPrimitive.Content
           data-slot="dialog-content"
           className={cn(
-            "bg-background fixed bottom-0 left-0 right-0 z-50 flex-col rounded-t-[10px] border-t p-6 h-full"
+            "bg-background fixed bottom-0 left-0 right-0 z-50 flex flex-col rounded-t-[10px] border-t p-6"
           )}
         >
-          <div className="scrollbar-hide flex-1 overflow-y-auto rounded-t-[10px] bg-inherit h-full">
+          <div className="scrollbar-hide flex-1 overflow-y-auto rounded-t-[10px] bg-inherit">
             {children}
           </div>
 
